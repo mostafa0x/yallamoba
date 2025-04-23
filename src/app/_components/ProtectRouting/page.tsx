6849f8d16716fea155cb50ec324c28375b1bda22
mostafa0x <dodomido12350@gmail.com>
2025-04-22 22:43:35 -0700

    Add Protect Routing

--- src/app/_components/ProtectRouting/page.tsx
+++ src/app/_components/ProtectRouting/page.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { StateFaces } from '../../../../InterFaces/StateFaces'
+import { usePathname, useRouter } from 'next/navigation'
 
-export default function ProtectRouting() {
-    return (
-        <div>ProtectRouting</div>
-    )
+export default function ProtectRouting({ children }: any) {
+    const Router = useRouter()
+    const Path = usePathname()
+    const { UserToken, UserData } = useSelector((state: StateFaces) => state.UserReducer)
+
+    useEffect(() => {
+
+        if (Path === "/profile") {
+            if (!UserToken) {
+                Router.push("/signin")
+            }
+        }
+
+    }, [Path])
+
+
+    return <div>
+        {children}
+    </div>
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 "use client";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import { Provider } from "react-redux";
 import { Store } from "@/lib/store";
 import NavBar from "./_components/NavBar/page";
-import "@fortawesome/fontawesome-free/css/all.min.css";
+import ProtectRouting from "./_components/ProtectRouting/page";
 
 export default function RootLayout({
   children,
@@ -16,7 +17,9 @@ export default function RootLayout({
       <Provider store={Store}>
         <body className="vsc-initialized">
           <NavBar />
-          {children}
+          <ProtectRouting>
+            {children}
+          </ProtectRouting>
         </body>
       </Provider>
     </html>
